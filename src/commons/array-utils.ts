a8455b2c2290d945c41e2097e7a496a4453b7904
Julien Coeurvolan <julien.coeurvolan@hellipse.fr>
2023-02-25 19:39:28 +0100

    export missing array function

--- src/commons/array-utils.ts
+++ src/commons/array-utils.ts
@@ -1,23 +1,23 @@
 export module ArrayUtils {
-  function pickRandomOne<T>(array: T[]): T {
+  export function pickRandomOne<T>(array: T[]): T {
     if (!array.length) {
       throw new Error("array is empty on pickRandom");
     }
     return array[Math.floor(Math.random() * array.length)];
   }
 
-  function removeItem<T>(current: T[], index: number): T[] {
+  export function removeItem<T>(current: T[], index: number): T[] {
     return current.filter((_, i) => i !== index);
   }
 
-  function fillWithIndex(size: number, start: number = 0): number[] {
+  export function fillWithIndex(size: number, start: number = 0): number[] {
     return new Array(size).fill(start).map((v, i) => v + i);
   }
 
-  function updateById<T>(current: T[], index: number, value: T): T[] {
+  export function updateById<T>(current: T[], index: number, value: T): T[] {
     return current.map((item, i) => (i === index ? value : item));
   }
-  function removeIdentical<T>(current: T[]): T[] {
+  export function removeIdentical<T>(current: T[]): T[] {
     return current.filter((v, i, self) => self.indexOf(v) === i);
   }
 
